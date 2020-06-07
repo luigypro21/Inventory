141d04bc24b3550cff9d0f3d805a47aba2d38dc0
luigypro21 <49296579+luigypro21@users.noreply.github.com>
2020-06-07 17:54:57 -0500

    Add files via upload

---
+++ DataAccess/Migrations/20200514043731_AddData.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class AddData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { "ASH", "Aseo Hogar" },
+                    { "ASP", "Aseo Personal" },
+                    { "HGR", "Hogar" },
+                    { "PRF", "Perfumería" },
+                    { "SLD", "Salud" },
+                    { "VDJ", "Video Juegos" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[,]
+                {
+                    { "280797e4-488a-4db1-b6e0-54377f825f07", "Calle 8 con 23", "Bodega Central" },
+                    { "9aee2e12-68ba-4932-b509-f2b391180a5d", "Calle norte y veintimilla", "Bodega Norte" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "ASH");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "ASP");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "HGR");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "PRF");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "SLD");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "VDJ");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "280797e4-488a-4db1-b6e0-54377f825f07");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "9aee2e12-68ba-4932-b509-f2b391180a5d");
+        }
+    }
+}
